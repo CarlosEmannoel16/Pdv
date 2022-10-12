509ea0d4d661839ed69856e137b10d4e53019b34
Carlos Emannoel <carlosemannoel2019@gmail.com>
2022-10-12 16:12:58 -0300

    test: criado o teste para body do signup

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -4,13 +4,34 @@ describe('Testanto Cadastro de Usuário', () => {
   test('Tem que da erro 400 ao não receber o => nome', () => {
     const sut = new SignUpController()
     const httpRequest = {
-      nome: 'nomeTest',
-      senha: 'senhaTeste',
-      cargo: 'cargoTest'
+      password: 'senhaTeste',
+      role: 'cargoTest'
     }
 
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Esta faltando oa parametros: name'))
+    expect(httpResponse.body).toEqual(new Error('Esta faltando os parametros: name'))
+  })
+  test('Tem que da erro 400 ao não receber  => password', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      name: 'nomeTest',
+      role: 'cargoTest'
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Esta faltando os parametros: password'))
+  })
+  test('Tem que da erro 400 ao não receber a => role', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      name: 'nomeTest',
+      password: 'senhaTeste'
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Esta faltando os parametros: role'))
   })
 })
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,7 +3,19 @@ export class SignUpController {
     if (!http.name) {
       return {
         statusCode: 400,
-        body: new Error('Esta faltando oa parametros: name')
+        body: new Error('Esta faltando os parametros: name')
+      }
+    }
+    if (!http.password) {
+      return {
+        statusCode: 400,
+        body: new Error('Esta faltando os parametros: password')
+      }
+    }
+    if (!http.role) {
+      return {
+        statusCode: 400,
+        body: new Error('Esta faltando os parametros: role')
       }
     }
   }
